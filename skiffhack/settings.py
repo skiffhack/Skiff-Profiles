87c0acfac604a787b7746a5c40b597bf923fe9e5
Thomas Parslow <tom@almostobsolete.net>
2012-01-13 18:37:07 +0000

    C'mon Heroku.

--- skiffhack/settings.py
+++ skiffhack/settings.py
@@ -32,25 +32,6 @@ USE_L10N = False
 
 LOGIN_REDIRECT_URL = '/manage/'
 
-urlparse.uses_netloc.append('postgres')
-urlparse.uses_netloc.append('mysql')
-try:
-    if os.environ.has_key('DATABASE_URL'):
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-        DATABASES['default'] = {
-            'NAME':     url.path[1:],
-            'USER':     url.username,
-            'PASSWORD': url.password,
-            'HOST':     url.hostname,
-            'PORT':     url.port,
-        }
-        if url.scheme == 'postgres':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except:
-    print "Unexpected error:", sys.exc_info()
-
 STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 STATIC_URL = '/static/'
 
